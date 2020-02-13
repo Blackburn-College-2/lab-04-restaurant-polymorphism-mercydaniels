e7c72196c5d1e36072527df646d61f1b4de2c424
mercydaniels <mercy.daniels@blackburn.edu>
2020-02-13 13:50:23 -0600

    implementation time

--- src/emptyjavaproject/Runner.java
+++ src/emptyjavaproject/Runner.java
@@ -1,9 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package emptyjavaproject;
+
+import edu.blackburn.cs.cs212.restaurantbase.*;
 
 /**
  *
@@ -15,7 +13,10 @@ public class Runner {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Size large = new Size("Large");
+        Money price =  new Money(500.00);
+        Coffee inAMug = new Coffee("In a Mug", large, price);
+        Receipt coffeeReceipt = new Receipt();
     }
     
 }
